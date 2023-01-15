5fd065e2a56422b17c806c03bd2866b550843a43
Gabriel Moya <gabrielmoya123@gmail.com>
2023-01-14 20:44:12 -0400

    Corrupção no código

---
+++ csharp/modelando-dominios-ricos/PaymentContext/PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,15 @@
+﻿using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests.Entities;
+
+[TestClass]
+public class StudentTests
+{
+    [TestMethod]
+    public void AdicionarAssinatura()
+    {
+        var subscription = new Subscription(null);
+        var student = new Student("Gabriel", "Moya", "12345678912", "hello@gabrielmoya.dev");
+        student.AddSubscription(subscription);
+    }
+}
