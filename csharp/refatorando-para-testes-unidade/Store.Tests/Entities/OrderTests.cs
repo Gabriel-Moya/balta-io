37beaf66a39717d61616d9f3bbb15fdb19c6366b
Gabriel Moya <gabrielmoya123@gmail.com>
2023-02-18 17:28:34 -0400

    Testando um pedido sem cliente

--- csharp/refatorando-para-testes-unidade/Store.Tests/Entities/OrderTests.cs
+++ csharp/refatorando-para-testes-unidade/Store.Tests/Entities/OrderTests.cs
@@ -113,6 +113,7 @@ public class OrderTests
     [TestCategory("Domain")]
     public void Dado_um_pedido_sem_cliente_o_mesmo_deve_ser_invalido()
     {
-        Assert.Fail();
+        var order = new Order(null, 10, _discount);
+        Assert.AreEqual(false, order.Valid);
     }
 }
