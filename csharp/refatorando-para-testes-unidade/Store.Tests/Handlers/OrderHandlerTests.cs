240c8bdf830b3713f1d593c4232b9eae186c6c0a
Gabriel Moya <gabrielmoya123@gmail.com>
2023-02-20 09:58:57 -0400

    Realizando os ToDos

--- csharp/refatorando-para-testes-unidade/Store.Tests/Handlers/OrderHandlerTests.cs
+++ csharp/refatorando-para-testes-unidade/Store.Tests/Handlers/OrderHandlerTests.cs
@@ -28,7 +28,15 @@ public class OrderHandlerTests
     public void Dado_um_cliente_inexistente_o_pedido_nao_deve_ser_gerado()
     {
         // TODO: Implementar
-        Assert.IsTrue(true);
+        var command = new CreateOrderCommand();
+        command.Customer = null;
+        command.ZipCode = "13411080";
+        command.PromoCode = "12345678";
+        command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+        command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+        command.Validate();
+
+        Assert.AreEqual(false, command.Valid);
     }
 
     [TestMethod]
@@ -36,7 +44,22 @@ public class OrderHandlerTests
     public void Dado_um_cep_invalido_o_pedido_deve_ser_gerado_normalmente()
     {
         // TODO: Implementar
-        Assert.IsTrue(true);
+        var command = new CreateOrderCommand();
+        command.Customer = "12345678";
+        command.ZipCode = null;
+        command.PromoCode = "12345678";
+        command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+        command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+
+        var handler = new OrderHandler(
+            _customerRepository,
+            _deliveryFeeRepository,
+            _discountRepository,
+            _productRepository,
+            _orderRepository);
+
+        handler.Handle(command);
+        Assert.AreEqual(true, handler.Valid);
     }
 
     [TestMethod]
@@ -44,7 +67,23 @@ public class OrderHandlerTests
     public void Dado_um_promocode_inexistente_o_pedido_deve_ser_gerado_normalmente()
     {
         // TODO: Implementar
-        Assert.IsTrue(true);
+        var command = new CreateOrderCommand();
+        command.Customer = "12345678";
+        command.ZipCode = "13411080";
+        command.PromoCode = null;
+        command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+        command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+
+        var handler = new OrderHandler(
+            _customerRepository,
+            _deliveryFeeRepository,
+            _discountRepository,
+            _productRepository,
+            _orderRepository);
+
+        handler.Handle(command);
+
+        Assert.AreEqual(true, handler.Valid);
     }
 
     [TestMethod]
@@ -52,7 +91,13 @@ public class OrderHandlerTests
     public void Dado_um_pedido_sem_itens_o_mesmo_nao_deve_ser_gerado()
     {
         // TODO: Implementar
-        Assert.IsTrue(true);
+        var command = new CreateOrderCommand();
+        command.Customer = "12345678";
+        command.ZipCode = "13411080";
+        command.PromoCode = "12345678";
+        command.Validate();
+
+        Assert.AreEqual(false, command.Valid);
     }
 
     [TestMethod]
