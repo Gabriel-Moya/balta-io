dd676277d7f04d8a3fc4072b51b6e759b87b16fa
Gabriel Moya <gabrielmoya123@gmail.com>
2023-02-25 14:08:54 -0400

    DataRow

--- csharp/aplicando-poo-projetos-reais-net7/UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
+++ csharp/aplicando-poo-projetos-reais-net7/UtmBuilder.Core.Tests/ValueObjects/UrlTests.cs
@@ -6,17 +6,41 @@ namespace UtmBuilder.Core.Tests.ValueObjects;
 [TestClass]
 public class UrlTests
 {
+    private const string InvalidUrl = "banana";
+    private const string ValidUrl = "https://balta.io";
+    
     [TestMethod]
     [ExpectedException(typeof(InvalidUrlException))]
     public void ShouldReturnExceptionWhenUrlIsInvalid()
     {
-        new Url("banana");
+        new Url(InvalidUrl);
     }
     
     [TestMethod]
     public void ShouldNotReturnExceptionWhenUrlIsInvalid()
     {
-        new Url("https://balta.io");
+        new Url(ValidUrl);
         Assert.IsTrue(true);
+    }
+    
+    [TestMethod]
+    [DataRow(" ", true)]
+    [DataRow("http", true)]
+    [DataRow("banana", true)]
+    [DataRow("https://balta.io", false)]
+    public void TestUrl(string link, bool expectException)
+    {
+        if (expectException)
+        {
+            try
+            {
+                new Url(link);
+                Assert.Fail();
+            }
+            catch (InvalidUrlException e)
+            {
+                Assert.IsTrue(true);
+            }
+        }
     }
 }
