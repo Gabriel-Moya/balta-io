66e77652facfeefc1e177b80ce756153a948c8f1
Gabriel Moya <gabrielmoya123@gmail.com>
2023-03-08 20:30:31 -0400

    Resolvendo as dependências - AddTransient

--- csharp/dominando-injecao-dependencia/01 - Inicial/DependencyStore/Program.cs
+++ csharp/dominando-injecao-dependencia/01 - Inicial/DependencyStore/Program.cs
@@ -1,5 +1,14 @@
+using DependencyStore.Repositories;
+using DependencyStore.Repositories.Contracts;
+using DependencyStore.Services;
+using DependencyStore.Services.Contracts;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+
+builder.Services.AddTransient<ICustomerRepository, CustomerRepository>();
+builder.Services.AddTransient<IPromoCodeRepository, PromoCodeRepository>();
+builder.Services.AddTransient<IDeliveryFeeService, DeliveryFeeService>();
 
 var app = builder.Build();
 
