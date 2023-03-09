749daee70c2bdefdfbd0a4688eef604a44c5354d
Gabriel Moya <gabrielmoya123@gmail.com>
2023-03-08 20:34:42 -0400

    Resolvendo as dependências - AddScoped

--- csharp/dominando-injecao-dependencia/01 - Inicial/DependencyStore/Program.cs
+++ csharp/dominando-injecao-dependencia/01 - Inicial/DependencyStore/Program.cs
@@ -2,10 +2,12 @@ using DependencyStore.Repositories;
 using DependencyStore.Repositories.Contracts;
 using DependencyStore.Services;
 using DependencyStore.Services.Contracts;
+using Microsoft.Data.SqlClient;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
+builder.Services.AddScoped<SqlConnection>(x => new SqlConnection(""));
 builder.Services.AddTransient<ICustomerRepository, CustomerRepository>();
 builder.Services.AddTransient<IPromoCodeRepository, PromoCodeRepository>();
 builder.Services.AddTransient<IDeliveryFeeService, DeliveryFeeService>();
