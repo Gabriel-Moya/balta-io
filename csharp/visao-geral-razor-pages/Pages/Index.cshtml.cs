55098d55916f07e04ff85a122dbd1f8879ecf0e3
Gabriel Moya <gabrielmoya123@gmail.com>
2023-03-17 16:00:30 -0400

    Criando um Razor Page

---
+++ csharp/visao-geral-razor-pages/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyRazorApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        public void OnPost() { }
+    }
+}
