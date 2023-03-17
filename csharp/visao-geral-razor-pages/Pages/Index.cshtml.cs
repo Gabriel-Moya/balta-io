6e6a75ca2ee35bcc0df0b026d7713f05e675492c
Gabriel Moya <gabrielmoya123@gmail.com>
2023-03-17 16:39:43 -0400

    Criando uma lista de categorias

--- csharp/visao-geral-razor-pages/Pages/Index.cshtml.cs
+++ csharp/visao-geral-razor-pages/Pages/Index.cshtml.cs
@@ -9,6 +9,10 @@ namespace MyRazorApp.Pages
         public async Task OnGet()
         {
             await Task.Delay(5000);
+            for (int i = 0; i <= 100; i++)
+            {
+                Categories.Add(new Category(i, $"Categoria {i}", i*18.95M));
+            }
         }
     }
 }
