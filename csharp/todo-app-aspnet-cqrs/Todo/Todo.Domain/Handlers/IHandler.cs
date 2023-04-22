fb9048b10f43f77b1c9dfb1d544ed487e282b861
Gabriel Moya <gabrielmoya123@gmail.com>
2023-04-21 21:15:01 -0400

    Contrato de handler

---
+++ csharp/todo-app-aspnet-cqrs/Todo/Todo.Domain/Handlers/IHandler.cs
@@ -0,0 +1,9 @@
+﻿using Todo.Domain.Commands.Contracts;
+
+namespace Todo.Domain.Handlers
+{
+    public interface IHandler<T> where T : ICommand
+    {
+        ICommandResult Handle(T command);
+    }
+}
