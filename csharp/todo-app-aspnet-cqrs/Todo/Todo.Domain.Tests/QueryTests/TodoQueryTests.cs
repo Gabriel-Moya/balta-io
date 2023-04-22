92f01bd6686744139635a69ca013d7361a8dca7a
Gabriel Moya <gabrielmoya123@gmail.com>
2023-04-22 14:42:31 -0400

    Testando as queries

---
+++ csharp/todo-app-aspnet-cqrs/Todo/Todo.Domain.Tests/QueryTests/TodoQueryTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Todo.Domain.Entities;
+using Todo.Domain.Queries;
+
+namespace Todo.Domain.Tests.QueryTests
+{
+    [TestClass]
+    public class TodoQueryTests
+    {
+        private List<TodoItem> _items;
+
+        public TodoQueryTests()
+        {
+            _items = new List<TodoItem>
+            {
+                new TodoItem("Tarefa 1", "UsuarioA", DateTime.Now),
+                new TodoItem("Tarefa 2", "UsuarioA", DateTime.Now),
+                new TodoItem("Tarefa 3", "moya", DateTime.Now),
+                new TodoItem("Tarefa 4", "UsuarioA", DateTime.Now),
+                new TodoItem("Tarefa 5", "moya", DateTime.Now)
+            };
+
+        }
+
+        [TestMethod]
+        public void Dada_a_consulta_deve_retornar_tarefas_apenas_do_usuario_moya()
+        {
+            var result = _items.AsQueryable().Where(TodoQueries.GetAll("moya"));
+            Assert.AreEqual(2, result.Count());
+        }
+    }
+}
