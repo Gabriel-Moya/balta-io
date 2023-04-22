074631025f41d925cb676cccd7fe05287564e994
Gabriel Moya <gabrielmoya123@gmail.com>
2023-04-21 22:38:52 -0400

    Fake repositories

--- csharp/todo-app-aspnet-cqrs/Todo/Todo.Domain.Tests/HandlerTests/CreateTodoHandlerTests.cs
+++ csharp/todo-app-aspnet-cqrs/Todo/Todo.Domain.Tests/HandlerTests/CreateTodoHandlerTests.cs
@@ -4,6 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using Todo.Domain.Commands;
 using Todo.Domain.Handlers;
+using Todo.Domain.Tests.Repositories;
 
 namespace Todo.Domain.Tests.HandlerTests
 {
@@ -14,7 +15,7 @@ namespace Todo.Domain.Tests.HandlerTests
         public void Dado_um_comando_invalido_deve_interromper_a_execucao()
         {
             var command = new CreateTodoCommand("", "", DateTime.Now);
-            var handler = new TodoHandler(null);
+            var handler = new TodoHandler(new FakeTodoRepository());
             Assert.Fail();
         }
 
