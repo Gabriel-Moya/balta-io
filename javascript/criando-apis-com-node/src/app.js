6f3bbe6120e2bf676f5f1e495a8dbd34a5391c93
Gabriel Moya <gabrielmoya123@gmail.com>
2023-07-16 13:14:33 -0300

    Connection string

--- javascript/criando-apis-com-node/src/app.js
+++ javascript/criando-apis-com-node/src/app.js
@@ -2,12 +2,14 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
 const app = express();
 const router = express.Router();
+dotenv.config();
 
-mongoose.connect('CONNECTION_STRING');
+mongoose.connect(`${process.env.CONNECTION_STRING}`);
 
 const indexRoute = require('./routes/index-route');
 const productRoute = require('./routes/product-route');
