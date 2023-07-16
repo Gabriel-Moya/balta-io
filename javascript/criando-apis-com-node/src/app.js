9738af1977b72473ec098de118bf9c99ba30b6ac
Gabriel Moya <gabrielmoya123@gmail.com>
2023-07-16 13:09:49 -0300

    Mongoose

--- javascript/criando-apis-com-node/src/app.js
+++ javascript/criando-apis-com-node/src/app.js
@@ -2,9 +2,12 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const router = express.Router();
+
+mongoose.connect('CONNECTION_STRING');
 
 const indexRoute = require('./routes/index-route');
 const productRoute = require('./routes/product-route');
