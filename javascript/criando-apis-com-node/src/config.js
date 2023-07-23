13f3a25b6d4821c45fcb8b19b7d9aa7a46707e2f
Gabriel Moya <gabrielmoya123@gmail.com>
2023-07-23 14:44:48 -0300

    Configuration file

---
+++ javascript/criando-apis-com-node/src/config.js
@@ -0,0 +1,8 @@
+global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
+global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
+
+module.exports = {
+  connectionString: 'YOUR_CONNECTION_STRING',
+  sendgridKey: 'YOUR_KEY',
+  containerConnectionString: 'YOUR_CONTAINER_CONNECTION_STRING'
+}
