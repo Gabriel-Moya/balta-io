0057aca5949dee52c88ee45f004c498408731613
Gabriel Moya <gabrielmoya123@gmail.com>
2024-09-28 17:20:32 -0400

    Refactor database connection and module imports

--- javascript/criando-apis-nest-framework/petshop/src/backoffice/backoffice.module.ts
+++ javascript/criando-apis-nest-framework/petshop/src/backoffice/backoffice.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
 import { CustomerController } from './controllers/customer.controller';
+import { UserSchema } from './schemas/user.schema';
+import { CustomerSchema } from './schemas/customer.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'Customer',
+        schema: CustomerSchema,
+      },
+      {
+        name: 'User',
+        schema: UserSchema,
+      }
+    ])
+  ],
   controllers: [CustomerController],
 })
 export class BackofficeModule {}
